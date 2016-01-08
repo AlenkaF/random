6bfa18bf8d51f327c67b801d5d8347d940a782d6
Alenka Frim <AlenkaF@users.noreply.github.com>
2016-01-08 13:18:43 +0100

    Create simple_imputations.py

---
+++ simple_imputations.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+# Setting up data frame with missing data (df)
+tmp = pd.DataFrame(np.random.randn(100,10))
+df = tmp[tmp>-1.5]
+
+# Short analysis of missing data
+df.describe()
+df.info()
+
+# SIMPLE IMPUTATIONS
+# Mean imputation
+df1 = df.fillna(df.mean())
+
+# Hot Deck (replacing missing with random values from data set)
+x = None
+while x is None:
+    x = df.sample(n=1)[pd.Series(np.random.randint(0, 9, size=1))].iloc[0] # Selecting random value from observed
+df2 = df.fillna(x.iloc[0]) 
