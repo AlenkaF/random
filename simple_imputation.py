53b9e3f256844ab91d2fd8b40f192450abf6525d
Alenka Frim <AlenkaF@users.noreply.github.com>
2016-01-09 22:27:21 +0100

    Update simple_imputation.py

--- simple_imputation.py
+++ simple_imputation.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-
-# ---------------------------
-# Using Pandas for imputation
-# (tesing)
-# ---------------------------
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
 # Setting up data frame with missing data (df)
 tmp = pd.DataFrame(np.random.randn(100,10))
@@ -21,9 +19,13 @@ df1 = df.fillna(df.mean())
 # Hot Deck (replacing missing with random values from data set)
 x = None
 while x is None:
-    x = df.sample(n=1)[pd.Series(np.random.randint(0, 9, size=1))].iloc[0] # Selecting random value from observed
+    x = df.sample(n=1)[pd.Series(np.random.randint(0, len(df.columns)-1, size=1))].iloc[0] # Selecting random value from observed
 df2 = df.fillna(x.iloc[0]) 
 
 # IMPUTED DATA ANALYSIS
 df1.describe()
 df2.describe()
+
+# Box-plot comparison per column
+ax=df.boxplot(return_type = 'axes')
+df1.boxplot(ax=ax)
